fd834ab6f5c9b368c53b8ed4d6019a79ff016a5b
Tyler Lyons <tlyons030@gmail.com>
2019-11-22 18:09:45 -0700

    step97

---
+++ Step97/Step97/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "My";
+            string string2 = "name";
+            string string3 = "is Tyler";
+            string concat = "" + string1 + " " + string2 + " " + string3 + "";
+            concat = concat.ToUpper();
+
+            Console.WriteLine(concat);
+
+            StringBuilder paragraph = new StringBuilder();
+            paragraph.Append("This is the first sentence of the paragraph.");
+            paragraph.Append("This is the second sentence of the paragraph.");
+            paragraph.Append("This is the third sentence of the paragraph.");
+            paragraph.Append("This is the last sentence of the paragraph.");
+            Console.WriteLine("\n"+paragraph+"");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Tyler";
+            //string quote = "The man said \"Hey\", "+name+". \non a new line. \t on a new tab";
+            //string fileName = @"C:\Users\Tyler";
+
+            //bool trueOrFalse = name.Contains("T");
+            //trueOrFalse = name.EndsWith("r");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Tyler");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
