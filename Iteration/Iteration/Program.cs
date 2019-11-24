36df2c228a26ba7e7f5e912e2fb25a5334da9407
Tyler Lyons <tlyons030@gmail.com>
2019-11-23 22:17:44 -0700

    step103

--- Iteration/Iteration/Program.cs
+++ Iteration/Iteration/Program.cs
@@ -8,12 +8,74 @@ class Program
     {
         static void Main(string[] args)
         {
-        int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 94 };
 
-        for (int i = 0; i < testScores.Length;)
+        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
 
-        Console.WriteLine();
+        //for (int i = 0; i < testScores.Length; i++)
+        //{
+        //    if (testScores[i] > 85)
+        //    {
+        //        Console.WriteLine("You Passed! Your score: " + testScores[i]);
+        //    }
+        //}
+
+        //string[] names = { "Tyler", "Davis", "Paul", "Sue" };
+
+        //for (int i = 0; i < names.Length; i++)
+        //{       
+        //    Console.WriteLine(names[i]); 
+        //}
+
+        //List<int> testScores = new List<int>();
+        //testScores.Add(98);
+        //testScores.Add(99);
+        //testScores.Add(81);
+        //testScores.Add(72);
+        //testScores.Add(70);
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score > 85)
+        //    {
+        //        Console.WriteLine("You Passed! Your score: " + score);
+        //    }
+        //}
+
+        //List<string> names = new List<string>() { "Tyler", "Davis", "Paul", "Sue" };
+
+        //foreach (string name in names)
+        //{
+        //    if (name.EndsWith("s"))
+        //    {
+        //        Console.WriteLine(name);
+        //    }
+        //}
+
+        List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+        List<int> passingScores = new List<int>();
+
+        foreach (int score in testScores)
+        {
+            if (score > 85)
+            {
+                passingScores.Add(score);
+            }
+        }
+
+        Console.WriteLine("Nunber of passing scores: " +passingScores.Count);
+
+        foreach (int score in testScores)
+        {
+            Console.WriteLine(score);
+        }
+
+        Console.WriteLine("\n");
+
+        foreach (int score in passingScores)
+        {
+            Console.WriteLine(score);
+        }
+
         Console.ReadLine();
         }
     }
-}
