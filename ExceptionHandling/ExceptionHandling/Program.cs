dd6a06b2ae74be5434c669be1ff5aeac749c63b8
Tyler Lyons <tlyons030@gmail.com>
2019-11-25 16:14:31 -0700

    step106

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nDividing the two");
+                int number3 = number1 / number2;
+                Console.WriteLine(number1 + " divided by " + number2 + "equals: " + number3);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
---
+++ Step106/Step106/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int> { 22, 53, 4, 2, 91 };
+                Console.WriteLine("Provide us a number please.");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in numbers)
+                {
+                    int result = number / userNumber;
+                    Console.WriteLine(number + " divided by " + userNumber + " equals: " + result);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("Program has emerged from the try/catch block");
+            Console.ReadLine();
+        }
+    }
+}
