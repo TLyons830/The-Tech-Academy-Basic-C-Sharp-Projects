5e4d9b491cc457f37625d8f08f7e201dea57f826
Tyler Lyons <tlyons030@gmail.com>
2019-12-05 16:07:17 -0700

    step111

---
+++ Step111/Step111/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step111
+{
+    public class Class1
+    {
+        public int UserInt;
+
+        public int SetUserInt (int uInt)
+        {
+            UserInt = uInt;
+            return UserInt;
+        }
+
+        public int methodOne()
+        {
+            int methodOneResult = UserInt + 1;
+            return methodOneResult;
+        }
+
+        public int methodTwo()
+        {
+            int methodTwoResult = UserInt - 1;
+            return methodTwoResult;
+        }
+
+        public int methodThree()
+        {
+            int methodTheeResult = UserInt * 2;
+            return methodTheeResult;
+        }
+
+
+
+    }
+}
---
+++ Step111/Step111/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 methods = new Class1();
+
+            Console.WriteLine("Please provide us an integer");
+            methods.SetUserInt(Convert.ToInt32(Console.ReadLine()));
+
+            int resultOne = methods.methodOne();
+            int resultTwo = methods.methodTwo();
+            int resultThree = methods.methodThree();
+
+            Console.WriteLine("Your integer + 1 is: "+resultOne+ "\nYour integer - 1 is: "+resultTwo+ "\nYour integer * 2 is: "+resultThree);
+            Console.ReadLine();
+
+        }
+    }
+}
