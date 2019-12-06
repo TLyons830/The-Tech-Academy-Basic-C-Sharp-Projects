ca97e40cec8f2e2a8bdfb201ff5b6632a0bbe816
Tyler Lyons <tlyons030@gmail.com>
2019-12-06 16:54:54 -0700

    step113

---
+++ Step113/Step113/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113
+{
+    public class Class1
+    {
+        public int i;
+        public int x;
+
+        public void mathOpp (int i, int x)
+        {
+            int addOne = i + 1;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Step113/Step113/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.i = 1;
+            class1.x = 2;
+
+            class1.mathOpp(4, 5);
+
+            class1.mathOpp(class1.i, class1.x);
+
+
+
+        }
+    }
+}
