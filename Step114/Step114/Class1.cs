3f6db1ebdb1ba624b4eb2bab28c0fec2c3e712b0
Tyler Lyons <tlyons030@gmail.com>
2019-12-06 17:37:18 -0700

    Step114Draft

---
+++ Step114/Step114/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step114
+{
+    public class Class1
+    {
+        public int i;
+
+        public int mathOpp(int i)
+        {
+            int mathOpp = i + 1;
+            return mathOpp;
+        }
+
+        public decimal mathOpp(decimal i)
+        {
+            decimal mathOpp = i * 2;
+            mathOpp = Convert.ToInt32(mathOpp);
+            return mathOpp;
+        }
+    }
+}
---
+++ Step114/Step114/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 classInt = new Class1();
+            classInt.i = 12;
+            int resultOne = classInt.mathOpp(classInt.i);
+            Console.WriteLine(resultOne);
+            Console.ReadLine();
+
+            Class1 classDec = new Class1();
+            classDec.i = Decimal.ToInt32(20.2M);
+            int resultTwo = classDec.mathOpp(classDec.i);
+            Console.WriteLine(resultTwo);
+            Console.ReadLine();
+
+
+        }
+    }
+}
