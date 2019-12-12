4d95f06d3a8f0b9a0cacef9f9bc67702affee6e1
Tyler Lyons <tlyons030@gmail.com>
2019-12-11 20:27:03 -0700

    Step137

---
+++ Step137/Step137/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number;
+            number.Amount = 22.22M;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
