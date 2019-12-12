d5f70ba1a6f1df5eb5af492a15b9493ddf97c27b
Tyler Lyons <tlyons030@gmail.com>
2019-12-11 18:11:02 -0700

    Step134

---
+++ Step134/Step134/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step134
+{
+    class Program
+    {
+        public enum daysofWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                string userDay = Console.ReadLine();
+                Console.WriteLine("You entered in that today is: " + Enum.Parse(typeof(daysofWeek), userDay));
+            }
+            catch
+            {
+                Console.WriteLine("Please enter in an actual day of the week, capitalizing the first letter.");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
