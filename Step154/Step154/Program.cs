bd772777745baf89e955716ceab0bc1f4181a991
Tyler Lyons <tlyons030@gmail.com>
2019-12-14 16:15:56 -0700

    step154

---
+++ Step154/Step154/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Step154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type a number");
+            string number = Convert.ToString(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\Tyler\Desktop\The-Tech-Academy-Basic-C-Sharp-Projects\Step154\log154.txt", number);
+
+            string printBack = File.ReadAllText(@"C:\Users\Tyler\Desktop\The-Tech-Academy-Basic-C-Sharp-Projects\Step154\log154.txt");
+            Console.WriteLine("\nReading number from log: {0}", printBack);
+            Console.ReadLine();
+        }
+    }
+}
