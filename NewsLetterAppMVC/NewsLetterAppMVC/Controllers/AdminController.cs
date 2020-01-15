0be8a8308bf3897a96c62939bd58660c7a9c5f98
Tyler Lyons <tlyons030@gmail.com>
2020-01-14 17:39:49 -0700

    Step339

---
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using NewsLetterAppMVC.Models;
+using NewsLetterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsLetterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterDBEntities db = new NewsletterDBEntities())
+            {
+                //var signUps = db.SignUps.Where(x => x.Removed == null).ToList();
+                var signUps = (from c in db.SignUps where c.Removed == null select c).ToList();
+                List<SignupVm> signupVms = new List<SignupVm>();
+                foreach (var signup in signUps)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterDBEntities db = new NewsletterDBEntities())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using NewsLetterAppMVC.Models;
+using NewsLetterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsLetterAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //private readonly string connectionString = @"Data Source=DESKTOP-PQ6SVM3\SQLEXPRESS;Initial Catalog=NewsletterDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (NewsletterDBEntities db = new NewsletterDBEntities())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES 
+                //                     (@FirstName, @LastName, @EmailAddress)";
+
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+
+                //    command.Parameters.Add("@FirstName", System.Data.SqlDbType.VarChar);
+                //    command.Parameters.Add("@LastName", System.Data.SqlDbType.VarChar);
+                //    command.Parameters.Add("@EmailAddress", System.Data.SqlDbType.VarChar);
+
+                //    command.Parameters["@FirstName"].Value = firstName;
+                //    command.Parameters["@LastName"].Value = lastName;
+                //    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                //    connection.Open();
+                //    command.ExecuteNonQuery();
+                //    connection.Close();
+                //}
+                return View("Success");
+            }
+        }
+
+
+                       
+            //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
+            //List<NewsletterSignUp> signUps = new List<NewsletterSignUp>();
+
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    SqlCommand command = new SqlCommand(queryString, connection);
+
+            //    connection.Open();
+
+            //    SqlDataReader reader = command.ExecuteReader();
+
+            //    while (reader.Read())
+            //    {
+            //        var signup = new NewsletterSignUp();
+            //        signup.Id = Convert.ToInt32(reader["Id"]);
+            //        signup.FirstName = reader["FirstName"].ToString();
+            //        signup.LastName = reader["LastName"].ToString();
+            //        signup.EmailAddress = reader["EmailAddress"].ToString();
+            //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+            //        signUps.Add(signup);
+            //    }
+            //}
+            
+    }
+}
